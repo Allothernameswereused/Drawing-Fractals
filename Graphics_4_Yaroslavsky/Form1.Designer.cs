7285f3a43385725fc8e389e1fadb89fd0b033f71
Allothernameswereused <63292134+Allothernameswereused@users.noreply.github.com>
2022-09-26 10:40:32 +0500

    Добавьте файлы проекта.

---
+++ Graphics_4_Yaroslavsky/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Graphics_4_Yaroslavsky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(812, 621);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Картинка";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(830, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(249, 163);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Управление";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(6, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 596);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(146, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "РИСОВАТЬ ФРАКТАЛ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(146, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ОЧИСТИТЬ ВСЁ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Глубина фрактала:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 645);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "RISOVALOCHKA 12K FRACTAL EDITION";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Graphics_4_Yaroslavsky/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics_4_Yaroslavsky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics gln;
+        private List<PointF> Initiator; //Стартовые точки фрактала
+        private float Scale; //Размер сегментов фрактала
+        private List<float> FractalGenerator; //Углы и расстояния для генератора фрактала
+        int depth; //Глубина фрактала
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                depth = int.Parse(textBox1.Text); //Пытается считать параметр глубины, введённый пользователем
+            }
+            catch
+            {
+                MessageBox.Show("Вы не ввели глубину фрактала (целое число)!");
+            }
+
+            gln = panel1.CreateGraphics();
+
+            Scale = 1 / 3f; //Размер сегментов фрактала - 1/3 от старшего сегмента
+
+            Initiator = new List<PointF>(); //Задаём координаты точек, используются для старта (видны при глубине 0)
+            float height = 0.75f * (Math.Min(panel1.Width, panel1.Height) - 20);
+            float width = (float)(height/Math.Sqrt(3.0)*2);
+            float y3 = panel1.Height - 10;
+            float y1 = y3 - height;
+            float x3 = panel1.Height / 2;
+            float x1 = x3 - width / 2;
+            float x2 = x1 + width;
+            Initiator.Add(new PointF(x1, y1));
+            Initiator.Add(new PointF(x2, y1));
+            Initiator.Add(new PointF(x3, y3));
+            Initiator.Add(new PointF(x1, y1));
+
+            FractalGenerator = new List<float>(); //Задаём углы линий при рисовании фрактала
+            float pi_over_3 = (float)(Math.PI / 3f);
+            FractalGenerator.Add(0);
+            FractalGenerator.Add(-pi_over_3);
+            FractalGenerator.Add(2*pi_over_3);
+            FractalGenerator.Add(-pi_over_3);
+
+
+            DrawFractal(gln, depth); //Рисуем фрактал с имеющимися данными
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Очищает холст
+        {
+            clearfield();
+        }
+
+
+        private void DrawFractal (Graphics gln, int depth) //Рисует фрактал
+        {
+            clearfield();
+
+            for (int counter = 1; counter < Initiator.Count; counter++)
+            {
+                PointF p1 = Initiator[counter - 1];
+                PointF p2 = Initiator[counter];
+
+                float dx = p2.X - p1.X;
+                float dy = p2.Y - p1.Y;
+                float length = (float)Math.Sqrt((dx * dx) + (dy * dy));
+                float theta = (float)Math.Atan2(dy, dx);
+                DrawEdge(gln, depth, ref p1, theta, length);
+            }
+
+        }
+
+        private void DrawEdge (Graphics gln, int depth, ref PointF p1, float theta, float dist) //Рекурсивная функция, рисует уголок фрактала
+        {
+            if (depth == 0)
+            {
+                PointF p2 = new PointF
+                    (
+                    (float)(p1.X + dist * Math.Cos(theta)),
+                    (float)(p1.Y + dist * Math.Sin(theta))
+                    );
+                gln.DrawLine(Pens.Black, p1, p2);
+                p1 = p2;
+                return;
+            }
+
+            dist *= Scale;
+            for (int counter =0; counter < FractalGenerator.Count; counter++)
+            {
+                theta += FractalGenerator[counter];
+                DrawEdge(gln, depth - 1, ref p1, theta, dist);
+            }
+        }
+
+        private void clearfield() //Очищает холст
+        {
+
+            SolidBrush mass_eraser = new SolidBrush(panel1.BackColor);
+            Rectangle field = new Rectangle(0, 0, panel1.Width, panel1.Height);
+            gln.FillRectangle(mass_eraser, field);
+        }
+
+    }
+}
